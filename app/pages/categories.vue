2d892c307d1d1886884d5a65bfcf1b429bf39060
Masum <masum@Masums-Mac-mini.local>
2026-01-15 13:57:22 +0600

    static menu

--- app/pages/categories.vue
+++ app/pages/categories.vue
@@ -1,5 +1,42 @@
 <script setup>
-const { data, pending, error } = await useFetch("/api/menus");
+const menus = [
+  { name: "Home", url: "/" },
+  {
+    name: "Products",
+    url: "/categories",
+    children: [
+      {
+        name: "Ethernet Router",
+        url: "ethernetrouter",
+        thumbnail: "/assets/images/ethernetrouter.avif",
+      },
+      {
+        name: "Wireless Router",
+        url: "wirelessrouter",
+        thumbnail: "/assets/images/wirelessrouter.avif",
+      },
+      {
+        name: "Access Point",
+        url: "accesspoint",
+        thumbnail: "/assets/images/accesspoint.avif",
+      },
+      {
+        name: "Switch",
+        url: "switch",
+        thumbnail: "/assets/images/switch.avif",
+      },
+      {
+        name: "Accessories",
+        url: "accessories",
+        thumbnail: "/assets/images/accessories.jpg",
+      },
+    ],
+  },
+
+  { name: "Installation Service", url: "/installation" },
+  { name: "Knowledge Base", url: "/knowledge" },
+  { name: "Contact", url: "/contact" },
+];
 
 useSeoMeta({
   title: "Product Categories",
@@ -14,17 +51,13 @@ useSeoMeta({
 
 <template>
   <div class="container py-12 mx-auto p-4">
-    <div v-if="pending">Loading...</div>
-
-    <div v-else-if="error">Failed to load products</div>
-
-    <section v-else>
+    <section>
       <Breadcrumb class="pb-6" />
       <div
         class="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
       >
         <NuxtLink
-          v-for="product in data.menus[1].children"
+          v-for="product in menus[1].children"
           :to="`/products/${product.url}`"
           :aria-label="product.name"
           class="bg-gray-100 rounded-lg shadow transform duration-300 hover:-translate-y-2 cursor-pointer"
